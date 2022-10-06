f0ffcf51818c119bff4eee5921024d88bda24fd5
devshadoww <dev.shadoww@gmail.com>
2022-10-06 21:58:23 +0530

    Add drawBoxUp() and drawBoxDown()

--- animating_in_c.c
+++ animating_in_c.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 static int x = 0;
-static int y = 100;
+static int y = 40;
 
 void drawBoxOne()
 {
@@ -65,15 +65,85 @@ void drawBoxRight()
   }
 }
 
+void drawBoxThree()
+{
+  int i;
+  for (i = 0; i < x; i++)
+    printf("\n");
+  printf("----------\n");
+  // for (i = 0; i < x; i++)
+  //   printf("\n");
+  printf("|        |\n");
+  // for (i = 0; i < x; i++)
+  //   printf("\n");
+  printf("|        |\n");
+  // for (i = 0; i < x; i++)
+  //   printf("\n");
+  printf("----------\n");
+  for (i = 0; i < x; i++)
+    printf("\n");
+  x++;
+}
+
+void drawBoxUp()
+{
+  int i, j, k;
+  for (i = 0; i < 100; i++)
+  {
+    drawBoxThree();
+    for (k = 0; k < 90000000; k++)
+      ;
+    for (j = 0; j < 50; j++)
+      printf("\n");
+  }
+}
+
+void drawBoxFour()
+{
+  int i;
+  for (i = y; i > 0; i--)
+    printf("\n");
+  printf("----------\n");
+  // for (i = y; i > 0; i--)
+  //   printf(" ");
+  printf("|        |\n");
+  // for (i = y; i > 0; i--)
+  //   printf(" ");
+  printf("|        |\n");
+  // for (i = y; i > 0; i--)
+  //   printf(" ");
+  printf("----------\n");
+  for (i = y; i > 0; i--)
+    printf("\n");
+  y--;
+}
+
+void drawBoxDown()
+{
+  int i, j, k;
+  for (i = 0; i < 100; i++)
+  {
+    drawBoxFour();
+    for (k = 0; k < 90000000; k++)
+      ;
+    for (j = 0; j < 50; j++)
+      printf("\n");
+  }
+}
+
 int main()
 {
   int choice = 0;
-  printf("1. Draw Box - 1  2. Draw Box - 2\n");
+  printf("1. Draw Box - 1  2. Draw Box - 2  3. Draw Box - 3  4. Draw Box - 4\n");
   printf("ENTER THE CHOICE : ");
   scanf("%d", &choice);
   if (choice == 1)
     drawBoxLeft();
   else if (choice == 2)
     drawBoxRight();
+  else if (choice == 3)
+    drawBoxUp();
+  else if (choice == 4)
+    drawBoxDown();
   return 0;
 }
