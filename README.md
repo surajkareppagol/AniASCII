84ea19b06aba4e48a478be73c431f2303917ae6b
shadoww <dev.shadoww@gmail.com>
2022-10-16 19:12:53 +0530

    Add new animations and Refactor code

--- README.md
+++ README.md
@@ -1,22 +1,3 @@
 # ANIMATION IN C WITHOUT ANY LIBRARIES
 
-This repository contains c program to perform basic animation without any libraries. At this stage very few animations are programmed but more will be added.
-
-## OUTPUT
-
-### Movement of box from right
-
-![output 1](img/img1.png)
-
-![output 2](img/img2.png)
-
-![output 3](img/img3.png)
-
-![output 4](img/img4.png)
-
-### Movement of the box from left
-
-![output 5](img/img5.png)
-
-![output 6](img/img6.png)
-
+Simple animation programs written in C language.
--- animating_in_c.c
+++ animating_in_c.c
@@ -1,149 +1,179 @@
 #include <stdio.h>
 
-static int x = 0;
-static int y = 40;
+void drawBoxRight();
+void drawBoxUp();
+void drawBoxDown();
+void drawBoxDiagonalRight();
+void drawBoxDiagonalLeft();
+void animateBox(int *);
 
-void drawBoxOne()
-{
-  int i;
-  for (i = 0; i < x; i++)
-    printf(" ");
-  printf("----------\n");
-  for (i = 0; i < x; i++)
-    printf(" ");
-  printf("|        |\n");
-  for (i = 0; i < x; i++)
-    printf(" ");
-  printf("|        |\n");
-  for (i = 0; i < x; i++)
-    printf(" ");
-  printf("----------\n");
-  x++;
-}
-
-void drawBoxLeft()
-{
-  int i, j, k;
-  for (i = 0; i < 100; i++)
-  {
-    drawBoxOne();
-    for (k = 0; k < 90000000; k++)
-      ;
-    for (j = 0; j < 50; j++)
-      printf("\n");
-  }
-}
-
-void drawBoxTwo()
-{
-  int i;
-  for (i = y; i > 0; i--)
-    printf(" ");
-  printf("----------\n");
-  for (i = y; i > 0; i--)
-    printf(" ");
-  printf("|        |\n");
-  for (i = y; i > 0; i--)
-    printf(" ");
-  printf("|        |\n");
-  for (i = y; i > 0; i--)
-    printf(" ");
-  printf("----------\n");
-  y--;
-}
-
-void drawBoxRight()
-{
-  int i, j, k;
-  for (i = 0; i < 100; i++)
-  {
-    drawBoxTwo();
-    for (k = 0; k < 90000000; k++)
-      ;
-    for (j = 0; j < 50; j++)
-      printf("\n");
-  }
-}
-
-void drawBoxThree()
-{
-  int i;
-  for (i = 0; i < x; i++)
-    printf("\n");
-  printf("----------\n");
-  // for (i = 0; i < x; i++)
-  //   printf("\n");
-  printf("|        |\n");
-  // for (i = 0; i < x; i++)
-  //   printf("\n");
-  printf("|        |\n");
-  // for (i = 0; i < x; i++)
-  //   printf("\n");
-  printf("----------\n");
-  for (i = 0; i < x; i++)
-    printf("\n");
-  x++;
-}
-
-void drawBoxUp()
-{
-  int i, j, k;
-  for (i = 0; i < 100; i++)
-  {
-    drawBoxThree();
-    for (k = 0; k < 90000000; k++)
-      ;
-    for (j = 0; j < 50; j++)
-      printf("\n");
-  }
-}
-
-void drawBoxFour()
-{
-  int i;
-  for (i = y; i > 0; i--)
-    printf("\n");
-  printf("----------\n");
-  // for (i = y; i > 0; i--)
-  //   printf(" ");
-  printf("|        |\n");
-  // for (i = y; i > 0; i--)
-  //   printf(" ");
-  printf("|        |\n");
-  // for (i = y; i > 0; i--)
-  //   printf(" ");
-  printf("----------\n");
-  for (i = y; i > 0; i--)
-    printf("\n");
-  y--;
-}
-
-void drawBoxDown()
-{
-  int i, j, k;
-  for (i = 0; i < 100; i++)
-  {
-    drawBoxFour();
-    for (k = 0; k < 90000000; k++)
-      ;
-    for (j = 0; j < 50; j++)
-      printf("\n");
-  }
-}
+static int forword = 0;
+static int forwordD = 0;
+static int backword = 40;
+static int backwordD = 40;
 
 int main()
 {
   int choice = 0;
-  printf("1. Draw Box - 1  2. Draw Box - 2  3. Draw Box - 3  4. Draw Box - 4\n");
+  printf("1. Draw Forword - 1  2. Draw Backword - 2  3. Draw Up - 3  4. Draw Down - 4 5. Draw Diagonal Right 6. Draw Diagonal Left\n");
   printf("ENTER THE CHOICE : ");
   scanf("%d", &choice);
-  if (choice == 1)
-    drawBoxLeft();
-  else if (choice == 2)
-    drawBoxRight();
-  else if (choice == 3)
-    drawBoxUp();
-  else if (choice == 4)
-    drawBoxDown();
+  animateBox(&choice);
   return 0;
 }
+
+///////////////////// BOX ONE
+void drawBoxRight()
+{
+  int i;
+  for (i = 0; i < forword; i++)
+    printf(" ");
+  printf("----------\n");
+  for (i = 0; i < forword; i++)
+    printf(" ");
+  printf("|        |\n");
+  for (i = 0; i < forword; i++)
+    printf(" ");
+  printf("|        |\n");
+  for (i = 0; i < forword; i++)
+    printf(" ");
+  printf("----------\n");
+  forword++;
+}
+/////////////////////
+
+///////////////////// BOX TWO
+void drawBoxLeft()
+{
+  int i;
+  for (i = backword; i > 0; i--)
+    printf(" ");
+  printf("----------\n");
+  for (i = backword; i > 0; i--)
+    printf(" ");
+  printf("|        |\n");
+  for (i = backword; i > 0; i--)
+    printf(" ");
+  printf("|        |\n");
+  for (i = backword; i > 0; i--)
+    printf(" ");
+  printf("----------\n");
+  backword--;
+}
+/////////////////////
+
+///////////////////// BOX THREE
+void drawBoxUp()
+{
+  int i;
+  for (i = 0; i < forword; i++)
+    printf("\n");
+  printf("----------\n");
+  printf("|        |\n");
+  printf("|        |\n");
+  printf("----------\n");
+  for (i = 0; i < forword; i++)
+    printf("\n");
+  forword++;
+}
+/////////////////////
+
+///////////////////// BOX FOUR
+void drawBoxDown()
+{
+  int i;
+  for (i = backword; i > 0; i--)
+    printf("\n");
+  printf("----------\n");
+  printf("|        |\n");
+  printf("|        |\n");
+  printf("----------\n");
+  for (i = backword; i > 0; i--)
+    printf("\n");
+  backword--;
+}
+/////////////////////
+
+///////////////////// BOX FIVE
+void drawBoxDiagonalRight()
+{
+  int i, j;
+  for (i = 0; i < forword; i++)
+    printf(" ");
+  printf("----------\n");
+
+  for (i = 0; i < forword; i++)
+    printf(" ");
+  printf("|        |\n");
+
+  for (i = 0; i < forword; i++)
+    printf(" ");
+  printf("|        |\n");
+
+  for (i = 0; i < forword; i++)
+    printf(" ");
+  printf("----------\n");
+  for (j = 0; j < forwordD; j++)
+    printf("\n");
+  forword++;
+  forwordD++;
+}
+/////////////////////
+
+///////////////////// BOX SIX
+void drawBoxDiagonalLeft()
+{
+  int i, j;
+  for (i = backword; i > 0; i--)
+    printf(" ");
+  printf("----------\n");
+  for (i = backword; i > 0; i--)
+    printf(" ");
+  printf("|        |\n");
+  for (i = backword; i > 0; i--)
+    printf(" ");
+  printf("|        |\n");
+  for (i = backword; i > 0; i--)
+    printf(" ");
+  printf("----------\n");
+  for (j = backwordD; j > 0; j--)
+    printf("\n");
+  backword--;
+  backwordD--;
+}
+/////////////////////
+
+void animateBox(int *choice)
+{
+  int i, j, k;
+  for (i = 0; i < 100; i++)
+  {
+    switch (*choice)
+    {
+    case 1:
+      drawBoxRight();
+      break;
+    case 2:
+      drawBoxLeft();
+      break;
+    case 3:
+      drawBoxUp();
+      break;
+    case 4:
+      drawBoxDown();
+      break;
+    case 5:
+      drawBoxDiagonalRight();
+      break;
+    case 6:
+      drawBoxDiagonalLeft();
+      break;
+    }
+    for (k = 0; k < 90000000; k++)
+      ;
+    for (j = 0; j < 50; j++)
+      printf("\n");
+  }
+}
+
+/////////////////////
