c86396cc185cfa2735c7bbd35a409bd0e1072cd2
shadoww <dev.shadoww@gmail.com>
2022-10-20 08:16:15 +0530

    Refactor code and make the animateBox() function dynamic

--- animating_in_c.c
+++ animating_in_c.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-void drawBoxRight();
-void drawBoxUp();
-void drawBoxDown();
-void drawBoxDiagonalRight();
-void drawBoxDiagonalLeft();
-void animateBox(int *);
+void drawBoxRight(int, int, char art[][12]);
+void drawBoxUp(int, int, char art[][12]);
+void drawBoxDown(int, int, char art[][12]);
+void drawBoxDiagonalRight(int, int, char art[][12]);
+void drawBoxDiagonalLeft(int, int, char art[][12]);
+void animateBox(int *, char art[][12]);
 
 static int forword = 0;
 static int forwordD = 0;
@@ -14,64 +14,55 @@ static int backwordD = 40;
 
 int main()
 {
+  char artBox[4][12] = {
+      {"----------"},
+      {"|        |"},
+      {"|        |"},
+      {"----------"}};
   int choice = 0;
-  printf("1. Draw Forword - 1  2. Draw Backword - 2  3. Draw Up - 3  4. Draw Down - 4 5. Draw Diagonal Right 6. Draw Diagonal Left\n");
+  printf("1. Draw Forword  2. Draw Backword  3. Draw Up 4. Draw Down 5. Draw Diagonal Right 6. Draw Diagonal Left\n");
   printf("ENTER THE CHOICE : ");
   scanf("%d", &choice);
-  animateBox(&choice);
+  animateBox(&choice, artBox);
   return 0;
 }
 
 ///////////////////// BOX ONE
-void drawBoxRight()
+void drawBoxRight(int arrCols, int arrRows, char art[4][12])
 {
-  int i;
-  for (i = 0; i < forword; i++)
-    printf(" ");
-  printf("----------\n");
-  for (i = 0; i < forword; i++)
-    printf(" ");
-  printf("|        |\n");
-  for (i = 0; i < forword; i++)
-    printf(" ");
-  printf("|        |\n");
-  for (i = 0; i < forword; i++)
-    printf(" ");
-  printf("----------\n");
+  int i, j;
+  for (i = 0; i < arrCols; i++)
+  {
+    for (j = 0; j < forword; j++)
+      printf(" ");
+    printf("%s\n", art[i]);
+  }
   forword++;
 }
 /////////////////////
 
 ///////////////////// BOX TWO
-void drawBoxLeft()
+void drawBoxLeft(int arrCols, int arrRows, char art[4][12])
 {
-  int i;
-  for (i = backword; i > 0; i--)
-    printf(" ");
-  printf("----------\n");
-  for (i = backword; i > 0; i--)
-    printf(" ");
-  printf("|        |\n");
-  for (i = backword; i > 0; i--)
-    printf(" ");
-  printf("|        |\n");
-  for (i = backword; i > 0; i--)
-    printf(" ");
-  printf("----------\n");
+  int i, j;
+  for (i = 0; i < arrCols; i++)
+  {
+    for (j = backword; j > 0; j--)
+      printf(" ");
+    printf("%s\n", art[i]);
+  }
   backword--;
 }
 /////////////////////
 
 ///////////////////// BOX THREE
-void drawBoxUp()
+void drawBoxUp(int arrCols, int arrRows, char art[4][12])
 {
-  int i;
+  int i, j;
   for (i = 0; i < forword; i++)
     printf("\n");
-  printf("----------\n");
-  printf("|        |\n");
-  printf("|        |\n");
-  printf("----------\n");
+  for (j = 0; j < arrCols; j++)
+    printf("%s\n", art[j]);
   for (i = 0; i < forword; i++)
     printf("\n");
   forword++;
@@ -79,15 +70,13 @@ void drawBoxUp()
 /////////////////////
 
 ///////////////////// BOX FOUR
-void drawBoxDown()
+void drawBoxDown(int arrCols, int arrRows, char art[4][12])
 {
-  int i;
+  int i, j;
   for (i = backword; i > 0; i--)
     printf("\n");
-  printf("----------\n");
-  printf("|        |\n");
-  printf("|        |\n");
-  printf("----------\n");
+  for (j = 0; j < arrCols; j++)
+    printf("%s\n", art[j]);
   for (i = backword; i > 0; i--)
     printf("\n");
   backword--;
@@ -95,55 +84,28 @@ void drawBoxDown()
 /////////////////////
 
 ///////////////////// BOX FIVE
-void drawBoxDiagonalRight()
+void drawBoxDiagonalRight(int arrCols, int arrRows, char art[4][12])
 {
   int i, j;
-  for (i = 0; i < forword; i++)
-    printf(" ");
-  printf("----------\n");
-
-  for (i = 0; i < forword; i++)
-    printf(" ");
-  printf("|        |\n");
-
-  for (i = 0; i < forword; i++)
-    printf(" ");
-  printf("|        |\n");
-
-  for (i = 0; i < forword; i++)
-    printf(" ");
-  printf("----------\n");
+  drawBoxRight(4, 4, art);
   for (j = 0; j < forwordD; j++)
     printf("\n");
-  forword++;
   forwordD++;
 }
 /////////////////////
 
 ///////////////////// BOX SIX
-void drawBoxDiagonalLeft()
+void drawBoxDiagonalLeft(int arrCols, int arrRows, char art[4][12])
 {
   int i, j;
-  for (i = backword; i > 0; i--)
-    printf(" ");
-  printf("----------\n");
-  for (i = backword; i > 0; i--)
-    printf(" ");
-  printf("|        |\n");
-  for (i = backword; i > 0; i--)
-    printf(" ");
-  printf("|        |\n");
-  for (i = backword; i > 0; i--)
-    printf(" ");
-  printf("----------\n");
+  drawBoxLeft(4, 4, art);
   for (j = backwordD; j > 0; j--)
     printf("\n");
-  backword--;
   backwordD--;
 }
 /////////////////////
 
-void animateBox(int *choice)
+void animateBox(int *choice, char artboxes[4][12])
 {
   int i, j, k;
   for (i = 0; i < 100; i++)
@@ -151,22 +113,22 @@ void animateBox(int *choice)
     switch (*choice)
     {
     case 1:
-      drawBoxRight();
+      drawBoxRight(4, 4, artboxes);
       break;
     case 2:
-      drawBoxLeft();
+      drawBoxLeft(4, 4, artboxes);
       break;
     case 3:
-      drawBoxUp();
+      drawBoxUp(4, 4, artboxes);
       break;
     case 4:
-      drawBoxDown();
+      drawBoxDown(4, 4, artboxes);
       break;
     case 5:
-      drawBoxDiagonalRight();
+      drawBoxDiagonalRight(4, 4, artboxes);
       break;
     case 6:
-      drawBoxDiagonalLeft();
+      drawBoxDiagonalLeft(4, 4, artboxes);
       break;
     }
     for (k = 0; k < 90000000; k++)
